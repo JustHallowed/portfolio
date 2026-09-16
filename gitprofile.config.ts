97aa238b6ae409c213f1a652f5c742a01ae36aa6
JustHallowed <yukanggogogo@gmail.com>
2026-09-16 21:09:44 +0800

    Update image url again

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -37,7 +37,7 @@ const CONFIG = {
           description:
             'You are a mask artist making ends meet by creating human disguises for monsters. Though business is lucrative, your customers can be quite... rowdy.',
           imageUrl:
-            '/MaskMotelBanner.png',
+            '/portfolio/MaskMotelBanner.png',
           link: 'https://justhallowed.itch.io/mask-motel',
         },
         {
